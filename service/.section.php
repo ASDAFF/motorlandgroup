526d648228bc640840662ff6d921f98f539253f5
neeil1990 <neeil@mail.ru>
2017-09-26 18:02:24 +0300

    no message

---
+++ service/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "service";
+$arDirProperties = Array(
+
+);
+?>
---
+++ service/index.php
@@ -0,0 +1,364 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("service");
+?>
+
+<div class="service">
+
+    <div class="service-menu">
+
+        <ul>
+
+            <li class="ser-menu">
+                <a href="">Модельный ряд </a>
+                    <div class="service-menu-sub offer">
+
+                        <div class="service-one-articles">
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+                            <ul>
+                                <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                                <li><p>Volvo провели что то где то и когда то</p></li>
+                                <li><p>Текст текст Текст текст Текст текст </p></li>
+                                <li><a href="#">Подробнее...</a></li>
+                            </ul>
+
+
+                        </div>
+
+                    </div>
+            </li>
+            <li class="ser-menu"><a href="">О компании</a></li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu"><a href="">Сервис</a></li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                            <li><a href="#">Чистка картера вентиляционных газов</a></li>
+                        </ul>
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                            <li><a href="#">Подробнее...</a></li>
+
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li><li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li><li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li><li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+            <li class="ser-menu">
+                <a href="">Конфигуратор</a>
+                <div class="service-menu-sub">
+
+                    <div class="service-one-articles">
+                        <ul>
+                            <li><img src="http://new.motorlandgroup.ru/upload/resize_cache/iblock/bab/520_360_2/bab63a6da33bfd2a7b0380faeab06181.jpg"></li>
+                        </ul>
+                        <ul>
+                            <li><h1>Volvo провели что то где то и когда то</h1></li>
+                            <li><p>Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст Текст текст </p></li>
+                            <li><a href="#">Подробнее...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </li>
+
+        </ul>
+
+    </div>
+
+
+
+
+</div>
+
+<script>
+    $(function(){
+        $('.service-menu > ul > li.ser-menu').hover(
+            function(e){
+
+                if($(this).find('.service-menu-sub').hasClass('offer')) {
+                    $(this).find('.service-menu-sub').css({'left':'0%','width':'100%'});
+                    $(this).find('.service-menu-sub ul').css({'width':'16.6%'});
+                }else{
+                    var offset = $(this).offset();
+                    var left = (100 * (offset.left) / $('body').width());
+                    if (left > 50) {
+                        left = 50;
+                    }
+                    $(this).find('.service-menu-sub').css('left', left + '%');
+                }
+                $(this).addClass('active');
+            },
+            function(){
+                $(this).removeClass('active');
+            }
+        );
+    });
+</script>
+
+<style>
+
+    .service-menu{
+        position: fixed;
+        width: 100%;
+        top: 70px;
+        background-color: rgba(22,22,24,.9);
+        z-index: 800;
+    }
+    .service-menu ul{
+        margin: 0;
+        padding: 0;
+    }
+    .service-menu ul li{
+        display: inline-block;
+        list-style: none;
+        min-width: 100px;
+        min-height: 50px;
+        padding: 0px 20px;
+    }
+    .service-menu ul li.active{
+        background: #fff;
+    }
+    .service-menu ul li.active a{
+        color: #333;
+    }
+    .service-menu ul li a{
+        font-size: 14px;
+        color: #fff;
+        text-decoration: none;
+        display: block;
+        text-align: center;
+        margin: 17px auto;
+    }
+
+    .service-menu ul li .service-menu-sub{
+        display: none;
+    }
+
+    .service-menu ul li.active .service-menu-sub{
+        position: absolute;
+        display: block;
+        background: #fff;
+        padding: 15px;
+        width: 50%;
+    }
+
+    .service-one-articles ul{
+        width: 50%;
+        float: left;
+    }
+
+
+
+</style>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+321
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
