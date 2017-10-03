d0ab3ef7ac49ce5494ba20ac5b490f4c49923345
info@prime-ltd.su <volvo-vrn@prime-ltd.su>
2017-10-03 11:56:44 +0300

    ser

--- include/service-banner.php
+++ include/service-banner.php
@@ -1,12 +1,11 @@
 <div class="service-banner">
 
-    <img src="/img/service.jpg">
+    <img src="/upload/iblock/6d1/6d16c801a49006f93f2c8ae48ee79aef.jpg">
     <div class="banner-text-two">
-        <h1>Текст Текст Текст</h1>
-        <p>Текст Текст ТекстТекстТекст Текст Текст Текст</p>
-        <p class="phone">9 (800) 555-55-44</p>
-        <p>Текст Текст ТекстТекст</p>
-        <a href="" class="button">Позвоните мне</a>
+        <h1>Узнать обо всем подробнее</h1>
+        <p>и записаться на сервис можно по телефону</p>
+        <p class="phone">(473) 23-303-23</p>
+        <a href=" #przv_call, #przv_email" class="button">Позвоните мне</a>
     </div>
 
 </div>
--- service/klientskaya-sluzhba/index.php
+++ service/klientskaya-sluzhba/index.php
@@ -35,7 +35,7 @@ $APPLICATION->SetTitle("kalkulyator-to");
 				<div class="page">
 					<div class="row">
 						<div class="form-new-detail">
-							<h1>Клиентская служба</h1>
+							<h1>Записаться на Шиномонтаж</h1>
 							 <?$APPLICATION->IncludeComponent(
 	"mystery:feedback.all.ajax",
 	"page.feedback.form",
@@ -52,7 +52,7 @@ $APPLICATION->SetTitle("kalkulyator-to");
 		"EMAIL_TO" => "zhukov@motorlandgroup.ru, savvina@motorlandgroup.ru, obrazhnikov@motorlandgroup.ru",
 		"EVENT_MESSAGE_ID" => array(0=>"8",),
 		"FIELDS" => array(0=>"NAME",1=>"PHONE",2=>"EMAIL",3=>"TIME",4=>"MODEL",),
-		"IBLOCK_ID" => "19",
+		"IBLOCK_ID" => "36",
 		"IBLOCK_TYPE" => "Feedback",
 		"ID_POPUP" => "feedback2",
 		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
@@ -72,25 +72,23 @@ $APPLICATION->SetTitle("kalkulyator-to");
                                 text-align: justify;
                             }
                         </style>
-					<p style="font-weight: bold;">
-						 При шиномонтаже в нашем сервисном центре комплексная мойка и проверка углов установки колес в подарок!
 					<p>
-					Деятельность всех отделов компании Мотор Ленд направлена на достижение общей цели Volvo – быть первыми среди автомобильных компаний в удовлетворении потребностей клиентов. Поэтому ваше мнение о качестве работы нашего дилерского центра очень важно для нас.<br>
+						 Деятельность всех отделов компании Мотор Ленд направлена на достижение общей цели Volvo – быть первыми среди автомобильных компаний в удовлетворении потребностей клиентов. Поэтому ваше мнение о качестве работы нашего дилерского центра очень важно для нас.<br>
+					</p>
 					<p style="font-weight: bold;">
-Если вы недовольны услугами, предоставленными вам в нашем салоне, обращайтесь с вопросами, замечаниями и пожеланиями непосредственно руководителю клиентской службе, Ирине Семеновой.<br>
+						 Если вы недовольны услугами, предоставленными вам в нашем салоне, обращайтесь с вопросами, замечаниями и пожеланиями непосредственно руководителю клиентской службе, Ирине Семеновой.<br>
 					</p>
 					<p>
-Тел. 8-920-405-20-25. <br>
-E-mail: <a href="mailto:isemenova@motorlandgroup.ru">isemenova@motorlandgroup.ru</a>
+						 Тел. 8-920-405-20-25. <br>
+						 E-mail: <a href="mailto:isemenova@motorlandgroup.ru">isemenova@motorlandgroup.ru</a>
 					</p>
 					<p style="text-align: center;">
-<span style="font-weight: bold;"><img src="/upload/klientskaya-sl.JPG" style="width:50%"><br>
- </span>
-<br>
-<br>
+ <span style="font-weight: bold;"><img src="/upload/klientskaya-sl.JPG" style="width:35%"><br>
+ </span> <br>
+ <br>
 					</p>
 					<p>
-С уважением, отдел по работе с клиентами Мотор Ленд.
+						 С уважением, отдел по работе с клиентами Мотор Ленд.
 					</p>
 				</div>
 			</div>
