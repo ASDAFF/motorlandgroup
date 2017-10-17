cf4b19f2524e8b1d95401760a48fe7040f18e11b
neeil1990 <neeil@mail.ru>
2017-10-17 11:23:17 +0300

    no message

--- include/service-banner.php
+++ include/service-banner.php
@@ -5,7 +5,7 @@
         <h1>Узнать обо всем подробнее</h1>
         <p>и записаться на сервис можно по телефону</p>
         <p class="phone">(473) 23-303-23</p>
-        <a href=" #przv_call, #przv_email" class="button">Позвоните мне</a>
+        <a href=" #przv_call" class="button">Позвоните мне</a>
     </div>
 
 </div>
